34e613bbc0f5b24bcdf130ffe17c1707d87612d5
tarunsingh8090 <65237201+tarunsingh8090@users.noreply.github.com>
2020-05-13 17:54:00 +0530

    Delete Problem 3.py

--- Day 1/Problem 3.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a="))
-b=int(input("enter b="))
-print("before swaping the value of a =",a)
-print("and the value of b=",b)
-a=a+b
-b=a-b
-a=a-b
-print("after swaping the value of a=",a)
-print("and the value of b=",b)
